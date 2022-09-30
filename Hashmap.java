a6398b5bc557c38af985e0cba01763841fe0f1a7
vladimir-confidentiality <markstevengeraga@gmail.com>
2022-09-30 10:44:06 +0800

    added HASHMAP

---
+++ Hashmap.java
@@ -0,0 +1,118 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hashmap;
+
+import java.util.*;
+import static java.lang.System.*;
+
+public class Hashmap {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Hashmap n = new Hashmap();
+        n.program();
+    }
+    HashMap<String, Integer> list = new HashMap<String, Integer>();
+    public void program() {
+
+        Scanner sc = new Scanner(System.in);
+        HashMap<String, Integer> list = new HashMap<String, Integer>();
+        out.println("A. Add number");
+        out.println("B. Remove number");
+        out.println("C. Display");
+        out.println("D. Clear number");
+        out.println("E. Exit");
+        out.println("...........................................................");
+        out.println("Choose letter above :");
+        out.println("");
+        char A, B, C, D, E;
+
+        String choice = sc.nextLine();
+        while (true) {
+
+            if (null == choice) {
+            } else {
+                switch (choice) {
+                    case "A" -> {
+                        addEntry();
+                        
+                    }
+                    case "B" -> {
+                    deleteEntry();
+                       
+                    }
+                    case "C" -> {
+                        showList();
+                        
+                    }
+                    case "D" -> {
+                        deleteList();
+                        
+                    }
+                    case "E" -> {
+                        System.out.println("The Program Is Terminated");
+                        return;
+                    }
+                }
+            }
+        }
+
+    }
+
+    /**
+     * void Add() {
+     * HashMap<Integer, Integer> hmap = new HashMap<Integer, Integer>();
+     * Scanner sc = new Scanner(System.in);
+     * out.println("input first number:");
+     * int a = sc.nextInt();
+     * out.println("input secodn number");
+     * int b = sc.nextInt();
+     * hmap.put(a, b);
+     * System.out.println(hmap);
+     * }
+     */
+
+    public void addEntry() {
+        Scanner sc = new Scanner(System.in);
+        HashMap<String, Integer> list = new HashMap<String, Integer>();
+        System.out.println("Add Entry/Word: ");
+        String article = sc.next();
+
+        System.out.println("Number: ");
+        int number = sc.nextInt();
+        list.put(article, number);
+        System.out.println("Added: " + list);
+        program();
+    }
+
+    public void showList() {
+        Iterator<String> it = list.keySet().iterator();
+        for (String key : list.keySet()) {
+            System.out.println(key + ": " + list.get(key));
+            System.out.println(list);
+            
+        }
+        program();
+    }
+
+    public void deleteEntry() {
+        System.out.println("Number of item you want to remove:");
+        Scanner sc = new Scanner(System.in);
+        int index = sc.nextInt();
+        index = index--;
+        int value = list.get(index);
+        list.remove(value);
+        program();
+    }
+
+    public void deleteList() {
+        list.clear();
+        program();
+    }
+
+}
