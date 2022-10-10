1d0ed0daeaddc1d1b61cca7020e5b8473720ac19
Hadrian-Evarula <96176285+Hadrian-Evarula@users.noreply.github.com>
2022-10-10 10:47:10 +0800

    Update

--- Main.java
+++ Main.java
@@ -1,65 +1,90 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        List<String> human = new ArrayList<String>();
+    public static void main(String[]args) {
 
-        Person<String> personFirst =new Person<String>();
-        personFirst.setMyName("Hadrian Evarula");
-        personFirst.setMyDetails("Good person");
-        personFirst.setMyVehicle("I love driving motorcycle");
-        human.add("\""+ personFirst.getMyName() + "\"" + " from "+ personFirst.getMyDetails() + " by: " + personFirst.getMyVehicle());
+        ArrayList<Person> person = new ArrayList<Person>();
 
+        Scanner input = new Scanner(System.in);
 
-        Person<String> personSecond =new Person<String>();
-        personSecond.setMyName("Haplas Tambal");
-        personSecond.setMyDetails("Healer person");
-        personSecond.setMyVehicle("I love driving Bike");   
-        human.add("\""+ personSecond.getMyName() + "\"" + " from "+ personSecond.getMyDetails() + " by: " + personSecond.getMyVehicle());     
+        System.out.print("Enter how many person you will input:");
+        int count = input.nextInt();
 
-        Person<String> personThird =new Person<String>();
-        personThird.setMyName("Sosiete Bedoria");
-        personThird.setMyDetails("Sister person");
-        personThird.setMyVehicle("I love driving DriveMax");  
-        human.add("\""+ personThird.getMyName() + "\"" + " from "+ personThird.getMyDetails() + " by: " + personThird.getMyVehicle());    
+        try {
 
-        Person<String> personFourth =new Person<String>();
-        personFourth.setMyName("JunJun Calesa");
-        personFourth.setMyDetails("Sister's of mary person");
-        personFourth.setMyVehicle("I love driving Hot Air balloon");      
-        human.add("\""+ personFourth.getMyName() + "\"" + " from "+ personFourth.getMyDetails() + " by: " + personFourth.getMyVehicle());
+            if (count > 5) {
+                boolean y;
+                while (y = true) {
+                    System.out.println("-----------------------------------");
+                    System.out.println("You're out of bound:");
+                    System.out.println("-----------------------------------");
+                    System.out.println("wanna try again?");
+                    String yes = input.next();
+                    if (yes == "yes") {
+                        y = true;
+                        return;
+                    } else {
+                        y = false;
+                        break;
+                    }
+                }
 
-        Person<String> personFifth =new Person<String>();
-        personFifth.setMyName("Ethyl Sotero");
-        personFifth.setMyDetails("Mother");
-        personFifth.setMyVehicle("I love riding Bus");
-        human.add("\""+ personFifth.getMyName() + "\"" + " from "+ personFifth.getMyDetails() + " by: " + personFifth.getMyVehicle());
+            } else {
 
-        System.out.print("Enter the index you want:");
-        int input=sc.nextInt();
+                int choice = -1;
 
-        try{
-            if(input < 0){
-                sc.close();
-                throw new PersonException("Your input is negative. Please input positive index.");
+                boolean f;
+
+                while (f = true) {
+                    System.out.println("------------------------------------");
+                    System.out.println("1.Input:"); 
+                    System.out.println("2.Display");
+                    System.out.println("3.Exit");
+                    System.out.println("------------------------------------");
+                    System.out.println("Enter your choice:");
+
+                    choice = input.nextInt();
+
+                    if (choice == 1) {
+                        System.out.println("Name:");
+                        String name = input.next();
+                        System.out.println("Age:");
+                        int age = input.nextInt();
+                        person.add(new Person(name, age));
+                    }
+
+                    else if (choice == 2) {
+
+                        System.out.println(person);
+
+                    } else if (choice == 3) {
+                        System.out.println("Thank you for participating!");
+                        System.out.println("you want to continue?");
+                        String u = input.next();
+                            if(u =="yes"){
+                                return;
+                            }else{
+                                f = false;
+                                break;
+                            }
+                        
+
+                    } else if (choice >= 4) {
+                       
+                        System.out.println("Out of bound try to input another number!");
+                        System.out.println("");
+                    }else if(choice ==0){
+                        System.out.println("Out of bound try to input another number!");
+                        System.out.println("");
+                    }
+                }
+
             }
-            if(input > 5){
-                sc.close();
-                throw new PersonException("Your input exceeds the size of array.");
-            }
 
-            System.out.println("Book Index: " + input);
-            System.out.println(human.get(input));
-
-        }
-        catch(PersonException e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
-        }
 
-        sc.close();
+        }
 
     }
 }
